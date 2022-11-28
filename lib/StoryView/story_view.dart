e63f24af7fea4ef441c4c951c7a9ea3ab59fa5e8
Yavuzalpguler <guler.yavuzalp@gmail.com>
2022-11-28 03:59:25 +0300

    story_view fix and hydrated bloc setup

--- lib/StoryView/story_view.dart
+++ lib/StoryView/story_view.dart
@@ -1,42 +1,65 @@
+import 'dart:async';
+
 import 'package:cube_transition_plus/cube_transition_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:instagram_story_clone/StoryView/components/story_item.dart';
+import 'package:instagram_story_clone/bloc/persist/stories_persist_bloc.dart';
+import 'package:instagram_story_clone/bloc/persist/stories_persist_event.dart';
+import 'package:instagram_story_clone/bloc/persist/stories_persist_model.dart';
 
 import '../data/story_data.dart';
 
 class StoryView extends StatefulWidget {
   const StoryView(
-      {super.key, required this.currentIndex, required this.details});
+      {super.key, required this.details, required this.currentStoryIndex});
 
-  final int currentIndex;
-  final UserStoryList details;
+  final List<UserStoryList>? details;
+  final int currentStoryIndex;
   @override
   State<StoryView> createState() => _StoryViewState();
 }
 
 class _StoryViewState extends State<StoryView> {
-  late PageController _pageController;
-  late int _currentIndex;
-
-  late UserStoryList _details;
+  late List<UserStoryList>? _details;
   @override
   void initState() {
     super.initState();
 
-    _currentIndex = widget.currentIndex;
     _details = widget.details;
-    _pageController = PageController(initialPage: _currentIndex);
   }
 
   @override
   void dispose() {
-    _pageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final PageController controller = PageController();
+    void initialPage() {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (controller.hasClients) {
+          controller.jumpToPage(widget.currentStoryIndex);
+        }
+      });
+    }
+
+    void scrollDownWithAnim(int index) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        controller.animateTo(
+          width * (index + 1),
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.fastOutSlowIn,
+        );
+      });
+    }
+
+    //Couldnt figure out how to add a call back to when te pageView is mounted, bad
+    Timer(const Duration(milliseconds: 100), () => initialPage());
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -44,43 +67,42 @@ class _StoryViewState extends State<StoryView> {
         child: SizedBox(
           height: height,
           child: CubePageView(
-            children: stories
-                .map((item) => StoryItemView(
-                      details: _details,
-                      currentIndex: _currentIndex,
-                    ))
-                .toList(),
+            controller: controller,
+            children: _details != null
+                ? _details!.asMap().entries.map((item) {
+                    return BlocProvider<CounterBloc>(
+                      create: (context) => CounterBloc(),
+                      child: BlocBuilder<CounterBloc, List<StoriesPersist>>(
+                          builder: (context, state) {
+                        return StoryItemView(
+                          details: item.value,
+                          currentIndex: state
+                              .firstWhere(
+                                  (element) => element.id == item.value.id,
+                                  orElse: () => StoriesPersist(
+                                      id: item.value.id, currentIndex: 0))
+                              .currentIndex,
+                          onPress: () {
+                            scrollDownWithAnim(item.key);
+                          },
+                          onTapDownBloc: (isForward, id) {
+                            BlocProvider.of<CounterBloc>(context).add(
+                              isForward
+                                  ? CounterIncrementPressed(id)
+                                  : CounterDecrementPressed(id),
+                            );
+                            // context
+                            //     .read<CounterBloc>()
+                            //     .add(CounterIncrementPressed());
+                          },
+                        );
+                      }),
+                    );
+                  }).toList()
+                : [],
           ),
         ),
       ),
-      // body: PageView.builder(
-      //   controller: _pageController,
-      //   itemCount: stories.length,
-      //   itemBuilder: (context, index) {
-      //     return StoryItemView(
-      //       stories: _stories,
-      //       currentIndex: _currentIndex,
-      //     );
-      //   },
-      // ),
     );
   }
 }
-
-
-// Center(
-//         child: SizedBox(
-//           height: height,
-//           child: CubePageView(
-//             children: places
-//                 .map(
-//                   (item) => Image.network(
-//                     item.url,
-//                     height: height,
-//                     fit: BoxFit.cover,
-//                   ),
-//                 )
-//                 .toList(),
-//           ),
-//         ),
-//       ),
