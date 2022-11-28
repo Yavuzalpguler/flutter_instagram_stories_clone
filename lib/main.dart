4acab3a1cc39b9a567f5212c00cb038ce668b35a
Yavuzalpguler <guler.yavuzalp@gmail.com>
2022-11-28 03:57:22 +0300

    hydrated pub main & clear storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:instagram_story_clone/bloc/bloc_provider.dart';
+import 'package:instagram_story_clone/bloc/stories_bar_bloc.dart';
 
 import 'package:instagram_story_clone/components/instagram_header.dart';
 import 'package:instagram_story_clone/components/stories_component.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
   runApp(const MyApp());
 }
 
@@ -22,26 +33,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
     var topPadding = mediaQueryData.padding.top;
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(top: topPadding / 2),
-        child: Column(
-          children: const [
-            InstagramHeader(),
-            StoriesBarWidget(),
-          ],
+    return BlocProvider<StoriesBarBloc>(
+      bloc: StoriesBarBloc(),
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(top: topPadding / 2),
+          child: Column(
+            children: [
+              const InstagramHeader(),
+              const StoriesBarWidget(),
+              const SizedBox(height: 80),
+              FloatingActionButton(
+                onPressed: () => HydratedBloc.storage.clear(),
+                tooltip: 'Clear Storage',
+                child: const Icon(Icons.delete_forever),
+              ),
+            ],
+          ),
         ),
       ),
     );
