dda90480a55c24e58d8ec9072d683bd4464ded93
Yavuzalpguler <guler.yavuzalp@gmail.com>
2022-11-28 03:58:48 +0300

    stories component with blocProvider

--- lib/components/stories_component.dart
+++ lib/components/stories_component.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_story_clone/bloc/bloc_provider.dart';
+import 'package:instagram_story_clone/bloc/stories_bar_bloc.dart';
 import 'package:instagram_story_clone/data/story_data.dart';
 import 'package:instagram_story_clone/models/story_model.dart';
 
@@ -9,11 +11,14 @@ import '../StoryView/story_view.dart';
 class StoryItem extends StatelessWidget {
   const StoryItem({
     super.key,
-    required this.storyIndex,
+    required this.storiesData,
     required this.size,
+    required this.currentStory,
   });
-  final int storyIndex;
   final double size;
+  final List<UserStoryList>? storiesData;
+  final UserStoryList currentStory;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,14 +29,14 @@ class StoryItem extends StatelessWidget {
                 barrierColor: Colors.black,
                 context: context,
                 builder: (context) => StoryView(
-                      currentIndex: 0,
-                      details: storyListUser[storyIndex],
+                      details: storiesData,
+                      currentStoryIndex: storiesData!.indexOf(currentStory),
                     ));
           },
           child: ClipRRect(
             borderRadius: BorderRadius.circular(size / 2),
             child: CachedNetworkImage(
-              imageUrl: storyListUser[storyIndex].user.profileImageUrl,
+              imageUrl: currentStory.user.profileImageUrl,
               width: size,
               height: size,
               fit: BoxFit.cover,
@@ -40,7 +45,7 @@ class StoryItem extends StatelessWidget {
         ),
         const SizedBox(height: 8),
         Text(
-          storyListUser[storyIndex].user.name,
+          currentStory.user.name,
           style: const TextStyle(
             fontWeight: FontWeight.bold,
           ),
@@ -57,21 +62,24 @@ class StoriesBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final StoriesBarBloc bloc = BlocProvider.of<StoriesBarBloc>(context);
+    bloc.searchQuery.add('Mock');
     return SizedBox(
       height: 105,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (BuildContext ctx, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                StoryItem(size: 60, storyIndex: index),
-              ],
+      child: StreamBuilder<List<UserStoryList>?>(
+        stream: bloc.storiesStream,
+        builder: (context, snapshot) => ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: snapshot.data?.length,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: StoryItem(
+              size: 70,
+              storiesData: snapshot.data,
+              currentStory: snapshot.data![index],
             ),
-          );
-        },
-        itemCount: storyListUser.length,
+          ),
+        ),
       ),
     );
   }
